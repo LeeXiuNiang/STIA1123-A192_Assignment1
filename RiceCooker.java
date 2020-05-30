7f83570f8f0b996f2902dce56b3502f9e7e99b4b
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-05-30 22:42:48 +0800

    Add files via upload

---
+++ RiceCooker.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class RiceCooker {
+
+   private String capacity;
+
+   //Overriding
+    public static void cookPlainRice() {
+        System.out.println("Start cooking plain rice.");
+    }
+
+    public static void heatRetention() {
+        System.out.println("Heat retention started.");
+    }
+
+    //Encapsulation
+    public void setCapacity(String newCapacity){
+        capacity = newCapacity;
+    }
+
+    public String getCapacity(){
+        return capacity;
+    }
+}
