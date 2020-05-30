b92f2e01a037d9cafc381e8d859693e4d4eefed0
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-05-30 22:44:21 +0800

    Create ClassicElectricRiceCooker.java

---
+++ ClassicElectricRiceCooker.java
@@ -0,0 +1,12 @@
+
+//Inheritance
+public class ClassicElectricRiceCooker extends RiceCooker{
+
+    String internalPot="Removable";
+    boolean nonStickySurface = true;
+
+    //Overriding
+    public static void cookPlainRice() {
+        System.out.println("Start cooking plain rice using classic electric rice cooker.");
+    }
+}
